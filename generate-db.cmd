a62cc1eecc7a849214ae912cbd0ea8358e45968b
Zdenek Devaty <zdenek.devaty@eliona.io>
2024-06-28 08:58:24 +0200

    Allow using COMMIT AND CHAIN in init script
    When initializing the db, we are in a transaction. Let's make the
    generation provide the same environment.

--- generate-db.cmd
+++ generate-db.cmd
@@ -4,13 +4,26 @@ go install github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-psql@latest
 go get github.com/volatiletech/sqlboiler/v4
 go get github.com/volatiletech/null/v8
 
+rem Read the content of init.sql
+set "INIT_SQL_CONTENT="
+for /f "delims=" %%i in ('type "%cd%\conf\init.sql"') do set "INIT_SQL_CONTENT=!INIT_SQL_CONTENT!%%i\n"
+
+rem Create init_wrapper.sql to run the script in a transaction. This is needed for
+rem COMMIT AND CHAIN to work in the script.
+(
+    echo BEGIN;
+    echo %INIT_SQL_CONTENT%
+    echo COMMIT;
+) > %cd%\conf\init_wrapper.sql
+
 docker run -d ^
     --name "app_sql_boiler_code_generation" ^
     -e "POSTGRES_PASSWORD=secret" ^
     -p "6001:5432" ^
-    -v "%cd%"\conf\init.sql:/docker-entrypoint-initdb.d/init.sql ^
+    -v "%cd%"\conf\init_wrapper.sql:/docker-entrypoint-initdb.d/init_wrapper.sql ^
     debezium/postgres:12  > NUL
 
+rem Wait for PostgreSQL to initialize
 timeout /t 5
 
 sqlboiler psql ^
